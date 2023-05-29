6743375ede1c4e5c200644a042007b8a8a659690
wadeking98 <wkingnumber2@gmail.com>
2023-05-29 11:01:19 -0700

    removed unused usecases
    Signed-off-by: wadeking98 <wkingnumber2@gmail.com>

--- server/src/content/student/StudentUseCases.ts
+++
@@ -1,8 +0,0 @@
-import type { CharWithUseCases } from '../types'
-
-import { Student, OnlineStore, StudyRoom } from '../../../config/student'
-
-export const StudentUseCases: CharWithUseCases = {
-  characterId: Student.id,
-  useCases: [OnlineStore, StudyRoom],
-}
