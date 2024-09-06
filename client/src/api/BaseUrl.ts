6245a9eaf4443ec27099d066814f8b8cb751ea90
Wade King <wkingnumber2@gmail.com>
2024-09-06 15:46:48 -0700

    quick fix (#252)
    Signed-off-by: wadeking98 <wkingnumber2@gmail.com>

--- client/src/api/BaseUrl.ts
+++ client/src/api/BaseUrl.ts
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
-export const baseUrl = (process.env.REACT_APP_HOST_BACKEND ?? '') + process.env.REACT_APP_BASE_ROUTE
-export const baseWsUrl = process.env.REACT_APP_HOST_BACKEND ?? ''
 export const baseRoute = process.env.REACT_APP_BASE_ROUTE ?? '/digital-trust/showcase'
+export const baseUrl = (process.env.REACT_APP_HOST_BACKEND ?? '') + baseRoute
+export const baseWsUrl = process.env.REACT_APP_HOST_BACKEND ?? ''
 export const socketPath = `${baseRoute}/demo/socket/`
 
 export const apiCall = axios.create({ baseURL: baseUrl })
