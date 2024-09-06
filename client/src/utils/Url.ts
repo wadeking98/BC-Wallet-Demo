6e15fdf53603e2b76012ff9301a18064d72eb365
Wade King <wkingnumber2@gmail.com>
2024-09-06 15:43:50 -0700

    fix for env variables (#251)
    Signed-off-by: wadeking98 <wkingnumber2@gmail.com>

--- client/src/utils/Url.ts
+++ client/src/utils/Url.ts
@@ -1,5 +1,5 @@
 export function prependApiUrl(path: string) {
-  let fullPath = `${process.env.REACT_APP_HOST_BACKEND ?? ''}${'/digital-trust/showcase'}${path}`
+  let fullPath = `${process.env.REACT_APP_HOST_BACKEND ?? ''}${process.env.REACT_APP_BASE_ROUTE}${path}`
 
   if (path.startsWith('data:')) {
     // path is a data url treat it as is
