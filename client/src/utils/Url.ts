ae877e9562ed9a61d7cbf0bad139cb10b17e631e
Wade King <wkingnumber2@gmail.com>
2024-09-06 16:02:16 -0700

    fix for images (#253)
    Signed-off-by: wadeking98 <wkingnumber2@gmail.com>

--- client/src/utils/Url.ts
+++ client/src/utils/Url.ts
@@ -1,5 +1,7 @@
+import { baseRoute } from "../api/BaseUrl"
+
 export function prependApiUrl(path: string) {
-  let fullPath = `${process.env.REACT_APP_HOST_BACKEND ?? ''}${process.env.REACT_APP_BASE_ROUTE}${path}`
+  let fullPath = `${process.env.REACT_APP_HOST_BACKEND ?? ''}${baseRoute}${path}`
 
   if (path.startsWith('data:')) {
     // path is a data url treat it as is
